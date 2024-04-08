f7e1feb7f3d0c7a404547939b1dff6359d86b48a
novedH <devonhuang11@gmail.com>
2024-04-08 14:29:38 -0700

    added our DE struct from our design submission

--- fsInit.c
+++ fsInit.c
@@ -20,12 +20,13 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #include "fsLow.h"
 #include "mfs.h"
 
 #define SIGNATURE 0x41444f4a353134 // 415JODA
-
+#define MAX_FILENAME_LEN 255
 
 typedef struct VCB{
 	uint64_t signature;
@@ -36,6 +37,17 @@ typedef struct VCB{
 };
 
 // TODO: Define Directory Entry (DE) Struct
+typedef struct DE
+{
+	char name[MAX_FILENAME_LEN]; // user-readable filename
+	time_t createTime;			 // timestamp for file creation time
+	time_t modTime;				 // timestamp for last modification time
+	time_t accessTime;			 // timestamp for last access time
+	int fileID;					 // a unique identifier for the file
+	int loc;					 // represents the location of the file on the disk
+	unsigned int size;			 // represents the size of the file in bytes
+	unsigned int isDir;			 // 0 if it is a file, 1 if it is a directory
+};
 
 unsigned char * freeSpaceMap;
 struct VCB *vcb;
@@ -72,11 +84,10 @@ int initFreeSpaceMap (uint64_t numberOfBlocks, uint64_t blockSize)
 
 	// Set first bit as used for VCB
 	setBit(0);
-
 	// Set bits used for freeSpaceMap
 	for (int i = 1; i <= blocksNeeded; i++)
 	{
-	setBit(i);
+		setBit(i);
 	}
 
 	LBAwrite(freeSpaceMap, blocksNeeded, 1);
@@ -159,6 +170,7 @@ int initFileSystem (uint64_t numberOfBlocks, uint64_t blockSize)
 	LBAwrite(vcb,1,0);
 
 	free(vcb);
+
 
 
 	return 0;
